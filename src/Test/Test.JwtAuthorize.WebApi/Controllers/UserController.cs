ce0a04d70797979e966ecff066f04f957d9c396f
yangliangbin <yangliangbin@hyihk.com>
2019-11-11 15:54:45 +0800

    add Test.JwtAuthorize.WebApi for test jwt

---
+++ src/Test/Test.JwtAuthorize.WebApi/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using XunZhi.Framework.JwtAuthorize;
+
+namespace Test.JwtAuthorize.WebApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private JwtClaimed _jwtClaimed;
+        public UserController(JwtClaimed jwtClaimed)
+        {
+            _jwtClaimed = jwtClaimed;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet("token")]
+        [AllowAnonymous]
+        public string GetToken()
+        {
+
+            return _jwtClaimed.CreateJwtToken(new JwtUser
+            {
+                UserID = 1,
+                NickName = "test",
+                UserChannel = 1,
+                UserType = 1
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [HttpGet("info")]
+        public JwtUser GetUser()
+        {
+            return _jwtClaimed.JwtUser;
+        }
+    }
+}
---
+++ src/Test/Test.JwtAuthorize.WebApi/Startup.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using XunZhi.Framework.JwtAuthorize;
+
+namespace Test.JwtAuthorize.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //jwt service
+            services.AddJwtAuthorize(Configuration);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            ///添加jwt验证
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
